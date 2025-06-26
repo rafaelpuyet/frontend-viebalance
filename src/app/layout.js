19360240b23ff412ca74eed9ec6fc3ab6b41108b
rafaelpuyet <79940385+rafaelpuyet@users.noreply.github.com>
2025-06-25 23:23:10 -0400

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,10 @@ const playfair = Playfair_Display({ subsets: ['latin'], weight: ['400', '700'] }
 export const metadata = {
   title: 'Clases de Pilates en Las Condes, Santiago | Vie Balance Pilates Studio',
   description: 'Únete a Vie Balance en Las Condes, Santiago, para clases de Pilates personalizadas. Regístrate para una prueba gratuita con Tamara Puyet hoy.',
+    icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
   openGraph: {
     title: 'Clases de Pilates en Las Condes | Vie Balance',
     description: 'Transforma tu salud con Pilates en Las Condes. Prueba gratuita con Tamara Puyet.',
@@ -28,6 +32,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="keywords" content="clases de Pilates Las Condes, Pilates Santiago, Pilates para principiantes, Vie Balance, Tamara Puyet" />
         <meta name="author" content="Vie Balance Pilates Studio" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="robots" content="index, follow" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet" />
